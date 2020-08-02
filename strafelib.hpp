d2a32600e5516fe7d681e8e21eb23fec12d16d14
Chong Jiang Wei <randolphchong@gmail.com>
2020-08-02 17:40:06 +0800

    Add squared speed routines

--- strafelib.hpp
+++ strafelib.hpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+/// The initial jumping speed, before gravity is applied.
 constexpr const double JUMP_SPEED = 268.3281572999748;
 
 /// Compute the speed after applying ground friction.
@@ -9,6 +10,10 @@ constexpr const double JUMP_SPEED = 268.3281572999748;
 /// This function runs at constant time. The caller is responsible of computing
 /// \p tau_k correctly by considering the values of the entity friction and
 /// the edgefriction.
+///
+/// If you are working on squared speeds for performance reasons, use fric_speedsq()
+/// instead to avoid computing square roots for \p speed when the geometric friction
+/// is in effect.
 double fric_speed(double speed, double E, double tau_k)
 {
     if (speed >= E) {
@@ -18,6 +23,31 @@ double fric_speed(double speed, double E, double tau_k)
     const double tau_E_k = tau_k * E;
     if (speed >= tau_E_k && speed >= 0.1) {
         return speed - tau_E_k;
+    }
+
+    return 0;
+}
+
+/// Compute the squared speed after applying ground friction.
+///
+/// This function behaves similar to fric_speed(), except it operates on the
+/// squared speed, making it more convenient to use if you are working on squared
+/// speeds for performance reasons (e.g. by relying on fme_maxaccel_speed_C()). Compared
+/// to fric_speed(), this function appears to be less performant when viewed in isolation.
+/// However, if you are working on squared speeds, this function avoids computing
+/// the square root for the most common case of geometric friction. Computing the
+/// arithmetic friction requires a square root.
+double fric_speedsq(double speedsq, double E, double tau_k)
+{
+    if (speedsq >= E * E) {
+        const double tmp = 1 - tau_k;
+        return speedsq * tmp * tmp;
+    }
+
+    const double tau_E_k = tau_k * E;
+    const double tau_E_k_sq = tau_E_k * tau_E_k;
+    if (speedsq >= tau_E_k_sq && speedsq >= 0.01) {
+        return speedsq - 2 * std::sqrt(speedsq) * tau_E_k + tau_E_k_sq;
     }
 
     return 0;
@@ -43,6 +73,9 @@ double fme_speed(double speed, double costheta, double L, double ke_tau_M_A)
 
 /// Compute the speed after applying the FME at maximum acceleration.
 ///
+/// This function is easier to use than fme_maxaccel_speed_C(), but much less
+/// performant for the common cases of zeta and 90 degrees strafing, due to
+/// the need to compute a square root.
 double fme_maxaccel_speed(double speed, double L, double ke_tau_M_A)
 {
     if (ke_tau_M_A >= 0) {
@@ -64,6 +97,53 @@ double fme_maxaccel_speed(double speed, double L, double ke_tau_M_A)
         return speed - ke_tau_M_A;
     }
     return speed;
+}
+
+/// Compute a constant for adding to the squared speed at maximum acceleration.
+///
+/// The new squared speed after applying the FME can always be written in the form
+/// of
+///
+/// \f[
+///   \lVert\mathbf{v}'\rVert^2 = \lVert\mathbf{v}\rVert^2 + C(\lVert\mathbf{v}\rVert^2)
+/// \f]
+///
+/// where \f$C\f$ is a function of the current squared speed in general. Though in
+/// the zeta and the 90 degrees cases, \f$C\f$ is independent of the current squared
+/// speed.
+///
+/// This function provides a significant performance boost over fme_maxaccel_speed() by
+/// avoiding the need to compute the relatively expensive square root for the most common
+/// cases of zeta and 90 degrees, at the expense of needing a square root when computing
+/// the much less common linear and backward-linear cases. Since \f$C\f$ is independent
+/// of the current speed in the zeta and 90 degrees cases, the value only needs to be
+/// computed once in those cases, assuming the caller is certain that the
+/// strafing type will never change. Subsequently, each iteration of the FME amounts only to
+/// a single addition of \f$C\f$ to the current squared speed.
+///
+/// It is the responsibility of the user to use this function correctly. If you are unsure
+/// how to use this function, opt for the more straightforward fme_maxaccel_speed() instead.
+double fme_maxaccel_speed_C(double speedsq, double L, double ke_tau_M_A)
+{
+    if (ke_tau_M_A >= 0) {
+        if (L <= ke_tau_M_A) {
+            if (L >= 0) {
+                return L * L;
+            }
+            return 0;
+        }
+
+        const double tmp = L - ke_tau_M_A;
+        if (tmp * tmp <= speedsq) {
+            return ke_tau_M_A * (L + tmp);
+        }
+        return (2 * std::sqrt(speedsq) + ke_tau_M_A) * ke_tau_M_A;
+    }
+
+    if (L >= 0 || L * L < speedsq) {
+        return (ke_tau_M_A - 2 * std::sqrt(speedsq)) * ke_tau_M_A;
+    }
+    return 0;
 }
 
 // /// Compute the speed after applying the FME at minimum acceleration.
--- test_strafelib.cpp
+++ test_strafelib.cpp
@@ -1,4 +1,5 @@
 #define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
 #include "strafelib.hpp"
 
@@ -24,10 +25,31 @@ TEST_CASE("fme maxaccel on speed", "[fme]") {
     SECTION("air at 100fps") {
         REQUIRE(fme_maxaccel_speed(700, 30, 32) == Approx(700.6425622241344));
     }
+    SECTION("air at 1000fps, zero initial speed") {
+        REQUIRE(fme_maxaccel_speed(0, 30, 3.2) == Approx(3.2));
+    }
     SECTION("air at 1000fps, A = -10") {
         REQUIRE(fme_maxaccel_speed(700, 30, -3.2) == Approx(703.2));
     }
     SECTION("ground at 250fps") {
         REQUIRE(fme_maxaccel_speed(400, 320, 12.8) == Approx(409.91238088157326));
+    }
+}
+
+TEST_CASE("fme maxaccel on speed C", "[fme]") {
+    SECTION("air at 1000fps") {
+        double C = fme_maxaccel_speed_C(1000 * 1000, 30, 3.2);
+        double new_speed = std::sqrt(1000 * 1000 + C);
+        REQUIRE(new_speed == Approx(1000.0908758707881));
+    }
+    SECTION("air at 1000fps, A = -10") {
+        double C = fme_maxaccel_speed_C(700 * 700, 30, -3.2);
+        double new_speed = std::sqrt(700 * 700 + C);
+        REQUIRE(new_speed == Approx(703.2));
+    }
+    SECTION("air at 1000fps, zero initial speed") {
+        double C = fme_maxaccel_speed_C(0, 30, 3.2);
+        double new_speed = std::sqrt(C);
+        REQUIRE(new_speed == Approx(3.2));
     }
 }
